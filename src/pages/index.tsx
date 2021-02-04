9c7d6aefc4f9f285db42bb6487882dc07b5f3232
lennonruthven <77502499+lennonruthven@users.noreply.github.com>
2021-02-04 01:18:55 +0000

    Add files via upload

---
+++ src/pages/index.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+import Header from '../components/header'
+import ExtLink from '../components/ext-link'
+import Features from '../components/features'
+import GitHub from '../components/svgs/github'
+import sharedStyles from '../styles/shared.module.css'
+
+export default () => (
+  <>
+    <Header titlePre="Home" />
+    <div className={sharedStyles.layout}>
+      <img
+        src="/vercel-and-notion.png"
+        height="85"
+        width="250"
+        alt="Vercel + Notion"
+      />
+      <h1>Hi, I'm Lennon!</h1>
+      <h2>
+        Welcome.'{' '}
+        <ExtLink
+          href="https://github.com/vercel/next.js/issues/9524"
+          className="dotted"
+          style={{ color: 'inherit' }}
+        >
+          SSG
+        </ExtLink>
+      </h2>
+
+      <Features />
+
+      <div className="explanation">
+        <p>
+        
+        </p>
+      </div>
+    </div>
+  </>
+)
